5babf661958d08ec928b1c59e9388d2e0e4e2d12
sailor generalmin <154836351+sailorgeneralmin@users.noreply.github.com>
2024-10-06 17:02:46 +0100

    first commit

---
+++ .history/index_20241005230835.js
@@ -0,0 +1,32 @@
+
+document.queryselector("item in html").AddEventListener("click, whatsapp");
+
+function whatsapp() {
+  fetch("https://wa.me/message/CXCUTSJ65XJ7K1");
+}
+
+document.querySelector("item  in html").AddEventListener("click, facebook");
+
+function facebook() {
+  fetch("link to facebook");
+}
+document.querySelector("item in html").AddEventListener("click, tiktok");
+
+function tiktok() {
+  fetch("https://www.tiktok.com/@shalewa.xxx?t=8pnxg8Vhbfu&_r=");
+}
+document.querySelector("item in html").AddEventListener("click, instagram");
+
+function instagram() {
+  Fetch(
+    "https://www.instagram.com/afaaf_lip_and_jewelries?igsh=a3F3dzc0OTVhM3Fm&utm_-source=qr"
+  );
+}
+
+function hver(element){
+  element.style.width="20%";
+}
+
+function nothover(element){
+  element.style.width=
+}
